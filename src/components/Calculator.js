e6a85e90c7f7b1632905d1874aee93d304e3518c
Zibilyonik <onatsurmeli@gmail.com>
2021-08-13 13:31:47 +0200

    add basic styling

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,7 +10,7 @@ class Calculator extends React.Component {
 
   render() {
     return (
-      <div id="Calculator" className="main row container w-50 w-lg-25 mx-auto my-5">
+      <div id="Calculator" className="main row container w-25 w-md-25 mx-auto my-5">
         <div className="text-input col-12 px-0"><input placeholder="0" type="text" id="CalculatorDisplay" className="w-100 text-end" /></div>
         <button type="button" className="calculator-buttons col-3 number-buttons">AC</button>
         <button type="button" className="calculator-buttons col-3 number-buttons">+/-</button>
